b2a68d921642f44a47cda9b1106ce39b2a19973e
VAKHANDER <vakhaevoa@mail.ru>
2022-03-27 12:02:47 +0300

    test2

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import pygame
 import sys
+# Обязательная строчка по всех программах - играх, для подготовки модулей к работе
 pygame.init()
+
+# Создаем окно screen с разрешением - 1280х720
 screen = pygame.display.set_mode((1280, 720))
-print("hello World")
+# загрузили изображение
+Menu1_surf = pygame.image.load('tiles\\Menu1.png')
+# Передали изображение в дисплей
+screen.blit(Menu1_surf, (0,0))
+
+while True:
+    for event in pygame.event.get():
+        # Обновили дисплей
+        pygame.display.update()
+        if event.type == pygame.QUIT:
+            # Завершаем работу модулей
+            pygame.quit()
+            # заврешаем работу программы
+            sys.exit()
