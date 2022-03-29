1771d14841e7b113bd727999bdfe1688db77a0ab
user <vakhaevoa@mail.ru>
2022-03-29 16:04:37 +0300

    created test func menu1()

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
 import sys
+
+def Menu1():
+    # загрузили изображение
+    Menu1_surf = pygame.image.load('tiles\\Menu1.png')
+    # Передали изображение в дисплей
+    screen.blit(Menu1_surf, (0, 0))
+
+
 # Обязательная строчка во всех программах - играх, для подготовки модулей к работе
 pygame.init()
 
 # Создаем окно screen с разрешением - 1280х720
 screen = pygame.display.set_mode((1280, 720))
-# загрузили изображение
-Menu1_surf = pygame.image.load('tiles\\Menu1.png')
-# Передали изображение в дисплей
-screen.blit(Menu1_surf, (0,0))
+Menu1()
 
 while True:
     # Обновили дисплей
